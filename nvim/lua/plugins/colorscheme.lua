813fa42b3392fa0c8d506f06302d81fc3ff055f5
gengdz <1583751445@qq.com>
2023-02-24 14:11:23 +0800

    chore: diagnostics undercurl

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,32 @@
 return {
+  -- 切换主题
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "everforest",
+  --   },
+  -- },
+
   {
     "catppuccin/nvim",
     lazy = true,
     enabled = false,
+  },
+
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      on_highlights = function(hl, c)
+        -- 不需要 下波浪线，因为颜色太杂 undercurl
+        hl.DiagnosticUnderlineWarn.undercurl = nil
+        hl.DiagnosticUnderlineWarn.underline = true
+        hl.DiagnosticUnderlineError.undercurl = nil
+        hl.DiagnosticUnderlineError.underline = true
+        hl.DiagnosticUnderlineInfo.undercurl = nil
+        hl.DiagnosticUnderlineInfo.underline = true
+        hl.DiagnosticUnderlineHint.undercurl = nil
+        hl.DiagnosticUnderlineHint.underline = true
+      end,
+    },
   },
 }
