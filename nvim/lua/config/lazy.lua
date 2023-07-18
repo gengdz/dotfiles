0fbd0abfe8b5c41d50d9a34013bfd214e2832846
gengdz <1583751445@qq.com>
2023-07-18 15:22:28 +0800

    chore(nvim): 尝试 lazy.extras，没生效

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -14,6 +14,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    -- prettier 使用下面的方式没生效
+    -- { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- import/override with your plugins
     { import = "plugins" },
   },
