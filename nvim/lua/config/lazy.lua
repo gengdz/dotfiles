b9b8981d459771afbfc6bb6577e0e3294fa5be77
gengdz <1583751445@qq.com>
2023-07-21 00:16:27 +0800

    feat(nvim): project

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -13,6 +13,7 @@ require("lazy").setup({
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.util.project" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- prettier 使用下面的方式没生效
     -- { import = "lazyvim.plugins.extras.formatting.prettier" },
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -10,30 +10,21 @@ return {
 
   {
     "goolord/alpha-nvim",
-    opts = function()
-      local dashboard = require("alpha.themes.dashboard")
+    optional = true,
+    opts = function(_, dashboard)
       local logo = [[]]
       dashboard.section.header.val = vim.split(logo, "\n")
-      dashboard.section.buttons.val = {
+      local buttons = {
         dashboard.button("e", " " .. " Explorer NeoTree", ":Neotree<CR>"),
         dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-        dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
-        dashboard.button("r", "󰄉 " .. " Recent files", ":Telescope oldfiles <CR>"),
-        dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
-        dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
-        dashboard.button("s", " " .. " Restore Session", [[:lua require("persistence").load() <cr>]]),
-        dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
-        dashboard.button("q", " " .. " Quit", ":qa<CR>"),
       }
-      for _, button in ipairs(dashboard.section.buttons.val) do
+      for _, button in ipairs(buttons) do
         button.opts.hl = "AlphaButtons"
         button.opts.hl_shortcut = "AlphaShortcut"
       end
-      dashboard.section.footer.opts.hl = "Type"
-      dashboard.section.header.opts.hl = "AlphaHeader"
-      dashboard.section.buttons.opts.hl = "AlphaButtons"
-      dashboard.opts.layout[1].val = 9
-      return dashboard
+      table.remove(dashboard.section.buttons.val, 1)
+      table.insert(dashboard.section.buttons.val, 1, buttons[1])
+      table.insert(dashboard.section.buttons.val, 2, buttons[2])
     end,
   },
 
