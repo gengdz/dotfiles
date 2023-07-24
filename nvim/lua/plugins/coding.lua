e1618876223e82751f38865d272d349ee23e732e
gengdz <1583751445@qq.com>
2023-07-24 23:16:02 +0800

    chore: 简化配置

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -22,9 +22,7 @@ return {
       opts.mapping = cmp.mapping.preset.insert({
         ["<C-b>"] = cmp.mapping.abort(),
         ["<C-f>"] = cmp.mapping.abort(),
-        ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.confirm({ select = true }),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
       })
     end,
   },
@@ -35,11 +33,6 @@ return {
       mappings = {
         add = "ys", -- Add surrounding in Normal and Visual modes
         delete = "ds", -- Delete surrounding
-        find = "gzf", -- Find surrounding (to the right)
-        find_left = "gzF", -- Find surrounding (to the left)
-        highlight = "sh", -- Highlight surrounding
-        replace = "cs", -- Replace surrounding
-        update_n_lines = "gzn", -- Update `n_lines`
       },
     },
   },
