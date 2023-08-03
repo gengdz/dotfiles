1f88c556b46e3fd73e5b9922b7ced88e4cb4e40c
gengdz <1583751445@qq.com>
2023-08-03 16:10:25 +0800

    chore: 不显示 .DS_store

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -10,6 +10,9 @@ return {
       filesystem = {
         filtered_items = {
           visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+          never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+            ".DS_Store",
+          },
         },
       },
     },
