8c6ffbb7f0119a8160d99a33fda7e1b84e386d24
gengdz <1583751445@qq.com>
2023-11-12 15:28:16 +0800

    chore(*): gitignore

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -48,4 +48,4 @@ vim.keymap.set("i", "<C-f>", "<Right>", opts)
 vim.keymap.set("i", "<C-a>", "<Home>", opts)
 
 vim.keymap.set("n", "<leader>wo", ":only<cr>", opts)
-vim.keymap.set("t", "<ESC>", "<C-\\><C-n>", opts)
+vim.keymap.set("t", "<ESC><ESC>", "<C-\\><C-n>", opts)
