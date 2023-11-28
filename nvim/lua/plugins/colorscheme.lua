5abe1cecb0e07956ec542c71279a3f87c69857a1
gengdz <1583751445@qq.com>
2023-11-28 23:40:11 +0800

    feat: alacritty

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -7,18 +7,5 @@ return {
 
   {
     "folke/tokyonight.nvim",
-    opts = {
-      on_highlights = function(hl, c)
-        -- 不需要 下波浪线，因为颜色太杂 undercurl
-        hl.DiagnosticUnderlineWarn.undercurl = nil
-        hl.DiagnosticUnderlineWarn.underline = true
-        hl.DiagnosticUnderlineError.undercurl = nil
-        hl.DiagnosticUnderlineError.underline = true
-        hl.DiagnosticUnderlineInfo.undercurl = nil
-        hl.DiagnosticUnderlineInfo.underline = true
-        hl.DiagnosticUnderlineHint.undercurl = nil
-        hl.DiagnosticUnderlineHint.underline = true
-      end,
-    },
   },
 }
