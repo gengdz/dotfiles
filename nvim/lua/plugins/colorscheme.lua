15e46138d1fbb9b398ad5c545b0ce55fc344a16f
gengdz <1583751445@qq.com>
2023-11-30 20:58:20 +0800

    feat(nvim): 更新 neovim 配置

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,42 @@
 return {
   {
-    "catppuccin/nvim",
-    lazy = true,
-    enabled = false,
-  },
-
-  {
     "folke/tokyonight.nvim",
+    priority = 1000,
+    opts = {
+      styles = {
+        -- variables = {
+        --   fg = "#E5C07B",
+        -- },
+      },
+      on_colors = function(c)
+        local util = require("tokyonight.util")
+        -- 遍历 table 并应用 darken 函数（不递归处理子 table）
+        local function darkenColors(colTable)
+          for k, v in pairs(colTable) do
+            if type(v) == "string" and v:sub(1, 1) == "#" then
+              -- 变暗十六进制的颜色值
+              colTable[k] = util.darken(v, 0.85)
+            end
+            -- 如果类型是 table，则跳过，不进行变暗处理
+          end
+        end
+
+        darkenColors(c)
+      end,
+      on_highlights = function(hl, c)
+        hl["@variable"] = {
+          fg = c.yellow,
+        }
+        hl["@parameter"] = {
+          fg = c.fg,
+        }
+      end,
+    },
   },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "tokyonight",
+  --   },
+  -- },
 }
