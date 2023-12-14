c07ecefd397366e6f06919136894d74ce1a6d670
gengdz <1583751445@qq.com>
2023-12-14 13:53:42 +0800

    feat(nvim): 简化自动补全的配置

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,31 +1,20 @@
 return {
   {
     "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/vim-vsnip",
-    },
     opts = function(_, opts)
       local cmp = require("cmp")
-      opts.sources = {
-        {
-          name = "nvim_lsp",
-          entry_filter = function(entry)
-            return require("cmp.types").lsp.CompletionItemKind[entry:get_kind()] ~= "Text"
-          end,
-        },
-        { name = "luasnip" }, -- For luasnip users.
-        { name = "vsnip" },
-        { name = "buffer" },
-        { name = "path" },
-      }
-
-      opts.mapping = cmp.mapping.preset.insert({
+      opts.mapping = vim.tbl_deep_extend("force", opts.mapping, {
         ["<C-b>"] = cmp.mapping.abort(),
         ["<C-f>"] = cmp.mapping.abort(),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.confirm({ select = true }),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
       })
+      -- table.insert(opts.sources, {
+      --   {
+      --     name = "nvim_lsp",
+      --     entry_filter = function(entry)
+      --       return require("cmp.types").lsp.CompletionItemKind[entry:get_kind()] ~= "Text"
+      --     end,
+      --   },
+      -- })
     end,
   },
   { "brglng/vim-im-select" },
