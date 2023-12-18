65c7a0cc4f6d8ab7d1af11a9a034195d3b620cb6
gengdz <1583751445@qq.com>
2023-12-18 11:01:35 +0800

    feat(nvim): 增加 pangu

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -14,14 +14,13 @@ vim.keymap.set("n", "<A-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increa
 vim.keymap.set("n", "<leader>q", ":q<cr>", opts)
 
 local formatOnSave = function()
-  -- -- 获取当前缓冲区的文件类型
-  -- local file_type = vim.bo.filetype
-  --
-  -- -- 如果文件类型是 Markdown (.md)
-  -- if file_type == "markdown" or file_type == "md" then
-  --   -- 调用 pangu 进行文本调整
-  --   vim.api.nvim_input("<cmd>PanguAll<cr><esc>")
-  -- end
+  -- 获取当前缓冲区的文件类型
+  local file_type = vim.bo.filetype
+
+  if file_type == "markdown" or file_type == "md" or file_type == "text" then
+    -- 调用 pangu 进行文本调整
+    vim.api.nvim_input("<cmd>PanguAll<cr><esc>")
+  end
   require("lazyvim.util").format({ force = true })
   vim.api.nvim_input("<cmd>w<cr><esc>")
 end
--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -1,1 +1,9 @@
-return {}
+return {
+  { "lukas-reineke/headlines.nvim", enabled = false },
+  {
+    "hotoo/pangu.vim",
+    ft = { "markdown", "text" },
+    lazy = true,
+    event = "VeryLazy",
+  },
+}
