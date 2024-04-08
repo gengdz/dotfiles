02b8a8d7d875a1bc66bf041f208e03aae9f9eb1d
gengdz <1583751445@qq.com>
2024-04-08 19:14:43 +0800

    feat(nvim): wsl 复制粘贴

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -28,8 +28,8 @@ vim.g.clipboard = {
     ["*"] = "clip.exe",
   },
   paste = {
-    ["+"] = [[powershell.exe -c "[Console]::Out.Write($(Get-Clipboard -Raw).Replace('\r', ''))"]],
-    ["*"] = [[powershell.exe -c "[Console]::Out.Write($(Get-Clipboard -Raw).Replace('\r', ''))"]],
+    ["+"] = [[powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))]],
+    ["*"] = [[powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))]],
   },
   cache_enabled = 0,
 }
