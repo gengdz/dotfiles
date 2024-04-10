31d991482582139fe5e66b554849c405ca9cdb12
gengdz <1583751445@qq.com>
2024-04-10 12:45:00 +0800

    feat(nvim): css-lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,5 +1,12 @@
 return {
   {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "css-lsp", "tailwindcss-language-server" })
+    end,
+  },
+  {
     "neovim/nvim-lspconfig",
     init = function()
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
