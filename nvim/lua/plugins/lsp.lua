9b6ad08624b44fb7b45c938d155c8833911de79d
gengdz <1583751445@qq.com>
2024-04-25 10:06:26 +0800

    feat(nvim): emmet-ls

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,7 +3,7 @@ return {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "css-lsp", "tailwindcss-language-server" })
+      vim.list_extend(opts.ensure_installed, { "css-lsp", "tailwindcss-language-server", "emmet-ls" })
     end,
   },
   {
