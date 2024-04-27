6fdeb0398dfa3d183ab76fab5d4fa8c2ed256738
gengdz <1583751445@qq.com>
2024-04-27 23:59:53 +0800

    feat(nvim): 'auto-tag'

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,9 @@ return {
   -- add more treesitter parsers
   {
     "nvim-treesitter/nvim-treesitter",
+    -- dependencies = {
+    --   { "windwp/nvim-ts-autotag" },
+    -- },
     opts = {
       highlight = {
         disable = { "json" },
@@ -13,8 +16,15 @@ return {
         disable = { "json" },
       },
       ensure_installed = {
+        "lua",
         "css",
         "scss",
+        "vimdoc",
+        "html",
+        "javascript",
+      },
+      autotag = {
+        enable = true,
       },
     },
   },
