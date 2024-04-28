a2611939173048378574df6c49144fcfa76c341e
gengdz <1583751445@qq.com>
2024-04-28 13:58:16 +0800

    chore(nvim): 删除无用代码

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -12,9 +12,7 @@ return {
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
       -- disable a keymap
       keys[#keys + 1] = { "<c-k>", false, mode = "i" }
-      -- keys[#keys + 1] = { "K", false }
       keys[#keys + 1] = { "gt", false }
-      -- keys[#keys + 1] = { "gh", vim.lsp.buf.hover, desc = "Hover" }
     end,
     opts = {
       servers = {
