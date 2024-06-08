7d0778036866fc453b1aecc91f63852efe111fa5
gengdz <1583751445@qq.com>
2024-06-08 09:25:09 +0800

    feat(nvim): 使用 h/l 操作文件目录

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -2,11 +2,6 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
-      window = {
-        mappings = {
-          ["o"] = "open",
-        },
-      },
       filesystem = {
         filtered_items = {
           visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
