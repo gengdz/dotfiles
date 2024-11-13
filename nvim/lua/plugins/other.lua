7b7fdf321603a5fee730d21c0bad293f41dba4a3
gengdz <1583751445@qq.com>
2024-11-13 10:11:02 +0800

    feat(nvim): snacks.nvim 使用 float termial

--- nvim/lua/plugins/other.lua
+++ nvim/lua/plugins/other.lua
@@ -1,7 +1,12 @@
--- every spec file under config.plugins will be loaded automatically by lazy.nvim
---
--- In your plugin files, you can:
--- * add extra plugins
--- * disable/enabled LazyVim plugins
--- * override the configuration of LazyVim plugins
-return {}
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      terminal = {
+        win = {
+          position = "float",
+        },
+      },
+    },
+  },
+}
