0ec0c3bbaeea06613c87b6ea361a95f608545da2
gengdz <1583751445@qq.com>
2024-12-10 10:40:01 +0800

    feat(nvim): im-select

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -20,35 +20,10 @@ return {
     end,
   },
   {
-    "hrsh7th/nvim-cmp",
-    opts = function(_, opts)
-      local cmp = require("cmp")
-      opts.mapping = vim.tbl_deep_extend("force", opts.mapping, {
-        ["<C-b>"] = cmp.mapping.abort(),
-        ["<C-f>"] = cmp.mapping.abort(),
-      })
-      table.insert(opts.sources, 1, {
-        name = "codeium",
-        group_index = 1,
-        priority = 1,
-      })
-    end,
-  },
-  {
     "keaising/im-select.nvim",
     opts = {
       default_command = "im-select",
       set_default_events = { "TermLeave", "VimEnter", "FocusGained", "InsertLeave", "CmdlineLeave" },
     },
   },
-  -- {
-  --   "echasnovski/mini.surround",
-  --   opts = function(_, opts)
-  --     opts.mappings = vim.tbl_deep_extend("force", opts.mappings, {
-  --       add = "ys", -- Add surrounding in Normal and Visual modes
-  --       delete = "ds", -- Delete surrounding
-  --       replace = "cs", -- Replace surrounding
-  --     })
-  --   end,
-  -- },
 }
