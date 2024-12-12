aee55850475d9b607c01a8a5b3a24b39244a3bc1
gengdz <1583751445@qq.com>
2024-12-12 10:33:00 +0800

    feat(nvim): 使用 emmet-language-server 解决 arbitrary tag names 的问题

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,7 +3,7 @@ return {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "css-lsp", "tailwindcss-language-server", "emmet-ls" })
+      vim.list_extend(opts.ensure_installed, { "css-lsp", "tailwindcss-language-server", "emmet-language-server" })
     end,
   },
   {
